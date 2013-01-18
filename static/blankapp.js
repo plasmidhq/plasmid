e7af537f7a82cc3d6012aab5b8239e5f051d9722
Calvin Spealman <ironfroggy@gmail.com>
2013-01-18 00:09:46 +0000

    Moving all the JS for the blankapp out of blankapp.html

--- static/blankapp.js
+++ static/blankapp.js
@@ -1,6 +1,6 @@
 define(function(require, exports, module) {
-
     plasmid = require('plasmid');
+
     appname = "APPNAME";
     plasmid_api = window.location.protocol + '//' + window.location.host + '/v1/';
     credentials = new plasmid.Credentials({api: plasmid_api});
@@ -25,6 +25,9 @@ define(function(require, exports, module) {
     exports.cred = credentials;
     exports.bootcred = bootstrap_credentials;
     exports.db = appdb;
+
+    $pairing = $('#pairing');
+    $pairing_inputs = $pairing.find('input');
     
     appdb.on('opensuccess', function() {
         // Do we already have credentials?
@@ -47,4 +50,36 @@ define(function(require, exports, module) {
         });
     });
 
+    appdb.on('credentialsready', function(cred){
+        var parts = [];
+        parts.push(cred.access.slice(0, 4));
+        parts.push(cred.access.slice(4, 8));
+        parts.push(cred.secret.slice(0, 4));
+        parts.push(cred.secret.slice(4, 8));
+        
+        for (var i=0; i < parts.length; i++) {
+            $pairing.find('input').eq(i).val(parts[i]);
+        }
+    });
+
+    $('#pairing-close').click(function() {
+        $pairing.hide();
+    });
+    $('#pairing-clear-and-pair').click(function() {
+        $pairing_inputs.val('');
+        $pairing_inputs.eq(0).focus();
+        $('#pairing-new-code').show();
+    });
+    $('#pairing-new-code').click(function() {
+        appdb.drop().then(function(){
+            var access = $pairing_inputs.eq(0).val() + $pairing_inputs.eq(1).val();
+            var secret = $pairing_inputs.eq(2).val() + $pairing_inputs.eq(3).val();
+            appdb.forgetPushed()
+            .then(function() {
+                appdb.meta('credentials', {
+                    access: access, secret: secret
+                })
+            });
+        });
+    });
 });
