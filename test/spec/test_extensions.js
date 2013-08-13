b43e9c4412c927bfd331e01c16c2230b3a250ef6
Calvin Spealman <ironfroggy@gmail.com>
2013-08-12 22:01:09 -0400

    [#10] First accurate tests of extension event delegation

--- test/spec/test_extensions.js
+++ test/spec/test_extensions.js
@@ -37,35 +37,29 @@ define(['plasmid.core', 'plasmid.ext'], function(plasmid, ext) {
       it('triggers correct extension events', function() {
         make_database(schema_ext);
 
-        var watcher = {
-          add_keys: [],
-          put_keys: [],
-
-          updated: function(store, action, key, value){
-          },
-
-          preupdated: function(store, action, key, value){
-            watcher[action + '_keys'].push(key);
-          }
-        };
-
-        created.on('storeupdate', watcher.updated);
-        //updated.on('storeupdate', watcher.updated);
-
-        created.on('storepreupdate', watcher.preupdated);
-        //updated.on('storepreupdate', watcher.preupdated);
+        var events = [];
+        created.on('storepreupdate', function(store, action, key, value) {
+          events.push({action:action, key:key, value:value});
+        });
 
         var p = make_queries('add X',
           function () {
             return DB.stores.notes.add('X', {note: 123});
+          },
+          function () {
+            return DB.stores.notes.put('Y', {note: 'abc'});
           }
-          //,function() {
-            //return DB.stores.notes.put('Y', {note: 'abc'});
-          //}
         );
         runs(function() {
-          expect(watcher.add_keys.length).toBe(1);
-          expect(watcher.add_keys[0]).toBe('X');
+          expect(events.length).toBe(2);
+
+          expect(events[0].action).toBe('add');
+          expect(events[0].key).toBe('X');
+          expect(events[0].value.note).toBe(123);
+
+          expect(events[1].action).toBe('put');
+          expect(events[1].key).toBe('Y');
+          expect(events[1].value.note).toBe('abc');
         });
 
       });
