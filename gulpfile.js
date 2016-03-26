bc14a9917aeb174c0de21ba500eef5af72d4fde7
Calvin Spealman <calvin@caktusgroup.com>
2016-03-26 16:59:15 -0400

    New gulp file building an empty module, start of moving modules over one by one.

---
+++ gulpfile.js
@@ -0,0 +1,131 @@
+var gulp = require('gulp');
+var source = require('vinyl-source-stream'); // Used to stream bundle for further handling
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babelify = require('babelify');
+var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
+var streamify = require('gulp-streamify');
+var notify = require('gulp-notify');
+var concat = require('gulp-concat');
+var gutil = require('gulp-util');
+var rename = require('gulp-rename');
+var connect = require('gulp-connect');
+var glob = require('glob');
+var path = require('path');
+var livereload = require('gulp-livereload');
+var child_exec = require('child_process').exec;
+
+var spawn = require('child_process').spawn;
+var argv = require('yargs')
+  .default('port', 8000)
+  .default('address', '0.0.0.0')
+  .argv;
+
+// External dependencies you do not want to rebundle while developing,
+// but include in your application deployment
+var dependencies = [
+];
+
+var browserifyTask = function (options) {
+
+  // Our app bundler
+  var appBundler = browserify({
+    entries: [options.src], // Only need initial file, browserify finds the rest
+    transform: [babelify], // We want to convert JSX to normal javascript
+    debug: options.development, // Gives us sourcemapping
+    cache: {}, packageCache: {}, fullPaths: options.development // Requirement of watchify
+  });
+
+  // We set our dependencies as externals on our app bundler when developing
+  (options.development ? dependencies : []).forEach(function (dep) {
+    appBundler.external(dep);
+  });
+
+  // The rebundle process
+  var rebundle = function () {
+    var start = Date.now();
+    console.log('Building APP bundle');
+    appBundler.bundle()
+      .on('error', gutil.log)
+      .pipe(source('index.js'))
+      .pipe(gulpif(!options.development, streamify(uglify())))
+      .pipe(rename('bundle.js'))
+      .pipe(gulp.dest(options.dest))
+      .pipe(gulpif(options.development, livereload()))
+      .pipe(notify(function () {
+        console.log('APP bundle built in ' + (Date.now() - start) + 'ms');
+      }));
+  };
+
+  // Fire up Watchify when developing
+  if (options.development) {
+    appBundler = watchify(appBundler);
+    appBundler.on('update', rebundle);
+  }
+
+  rebundle();
+
+  // We create a separate bundle for our dependencies as they
+  // should not rebundle on file changes. This only happens when
+  // we develop. When deploying the dependencies will be included
+  // in the application bundle
+  if (options.development) {
+
+    var vendorsBundler = browserify({
+      debug: true,
+      require: dependencies
+    });
+
+    // Run the vendor bundle
+    var start = new Date();
+    console.log('Building VENDORS bundle');
+    vendorsBundler.bundle()
+      .on('error', gutil.log)
+      .pipe(source('vendors.js'))
+      .pipe(gulpif(!options.development, streamify(uglify())))
+      .pipe(gulp.dest(options.dest))
+      .pipe(notify(function () {
+        console.log('VENDORS bundle built in ' + (Date.now() - start) + 'ms');
+      }));
+  }
+}
+
+function rebuild(options) {
+  var options = options || {};
+
+  browserifyTask({
+    development: options.development,
+    src: './src/index.js',
+    dest: './build/'
+  });
+}
+
+
+gulp.task('jsdoc', function (cb) {
+  child_exec('node ./node_modules/jsdoc/jsdoc.js -c ./.jsdoc.json -r ./rescue_id/static/js -d ./docs/js/', undefined, cb);
+});
+
+
+gulp.task('default', function (cb) {
+  livereload.listen();
+
+  rebuild({
+    development: true,
+  });
+
+  connect.server();
+
+});
+
+gulp.task('build', function() {
+  rebuild({
+    development: true,
+  })
+});
+
+gulp.task('deploy', function() {
+  rebuild({
+    development: false,
+  })
+});
--- karma.conf.js
+++ karma.conf.js
@@ -1,71 +1,70 @@
 // Karma configuration
+// Generated on Tue Mar 08 2016 10:58:13 GMT-0500 (EST)
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+module.exports = function(config) {
+  config.set({
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  REQUIRE,
-  REQUIRE_ADAPTER,
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
 
-  {pattern: 'src/*.js', included: false},
-  {pattern: 'src/**/*.js', included: false},
 
-  {pattern: 'test/util/*.js'},
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
 
-  {pattern: 'test/spec/test_*.js', included: false},
 
-  'test/main.js',
-];
+    // list of files / patterns to load in the browser
+    files: [
+      'src/*.js',
+      'test/spec/*.js'
+    ],
 
-// list of files to exclude
-exclude = [];
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
+    // list of files to exclude
+    exclude: [
+    ],
 
-// web server port
-port = 8080;
 
-// cli runner port
-runnerPort = 9100;
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
+    // web server port
+    port: 9876,
 
-browsers = [
-    'Firefox',
-];
-var isWin = !!process.platform.match(/^win/);
-if (isWin) {
-    browsers.push('IE');
-    browsers.push('Chrome');
-} else {
-    browsers.push('/usr/bin/chromium-browser');
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
 }
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 30000;
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
