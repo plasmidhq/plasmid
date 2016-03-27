e2bfcde2d72f7ea32f32bdc76910498f28b25526
Calvin Spealman <calvin@caktusgroup.com>
2016-03-27 11:53:43 -0400

    Adding issing index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var core = require('./plasmid.core.js');
+var util = require('./plasmid.utilities.js');
+
+exports.Credentials = core.Credentials;
+exports.Database = core.Database;
+exports.LocalStore = core.LocalStore;
+exports.SyncStore = core.SyncStore;
+exports.EventListener = core.EventListener;
+exports.Promise = core.Promise;
+exports.http = util.http;
